2402007e7fbbfe98a560bbcf5693125f62f97c66
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-09-24 14:31:52 +0200

    oop lab 01

---
+++ Laboratorio/oop-lab01/02-fill-class-student/Student.java
@@ -0,0 +1,16 @@
+class Student {
+
+    // È buona pratica mettere i campi in testa alla classe
+
+    void build(/* Qui vanno i parametri necessari a inizializzare l'oggetto */) {
+        /*
+         * Completare il corpo del metodo
+         */
+    }
+
+    void printStudentInfo() {
+        /*
+         * Completare il corpo del metodo
+         */
+    }
+}
---
+++ Laboratorio/oop-lab01/02-fill-class-student/TestStudent.java
@@ -0,0 +1,8 @@
+class TestStudent {
+
+    public static void main(String[] args) {
+        Student someone = new Student();
+        someone.build();
+        someone.printStudentInfo();
+    }
+}
