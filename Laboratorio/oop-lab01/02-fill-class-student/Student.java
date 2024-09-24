27e9a3db0769960f657b63c50883c7f8a15ec709
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-09-24 15:00:40 +0200

    es 2

--- Laboratorio/oop-lab01/02-fill-class-student/Student.java
+++ Laboratorio/oop-lab01/02-fill-class-student/Student.java
@@ -2,15 +2,29 @@ class Student {
 
     // È buona pratica mettere i campi in testa alla classe
 
-    void build(/* Qui vanno i parametri necessari a inizializzare l'oggetto */) {
+    String name;
+    String surname;
+    int id;
+    int matriculationYear;
+
+    void build(String name, String surname, int id, int matriculationYear) {
         /*
          * Completare il corpo del metodo
          */
+
+        this.name = name;
+        this.surname = surname;
+        this.id = id;
+        this.matriculationYear = matriculationYear;
     }
 
     void printStudentInfo() {
         /*
          * Completare il corpo del metodo
          */
+
+        System.out.println(this.name + " " + this.surname
+                + "(id: " + this.id
+                + ", matriculation year: " + this.matriculationYear + ")");
     }
 }
