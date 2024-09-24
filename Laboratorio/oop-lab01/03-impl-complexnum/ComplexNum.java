c023daf07fcc715e75b86cb6303bfb3741086ab2
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-09-24 15:23:06 +0200

    es 3

--- Laboratorio/oop-lab01/03-impl-complexnum/ComplexNum.java
+++ Laboratorio/oop-lab01/03-impl-complexnum/ComplexNum.java
@@ -3,10 +3,16 @@ class ComplexNum {
      * Inserire qui la dichiarazione dei campi della classe
      */
 
+    double re;
+    double im;
+
     void build(double re, double im) {
         /*
          * Inserire qui l'inizializzazione dei campi con i parametri forniti in input
          */
+
+        this.re = re;
+        this.im = im;
     }
 
     boolean equal(ComplexNum num) {
@@ -14,7 +20,8 @@ class ComplexNum {
          * Implementare il metodo in modo che restituisca true se e solo se il
          * numero complesso è uguale al parametro num passato in input
          */
-        return false;
+
+        return this.re == num.re && this.im == num.im;
     }
 
     void add(ComplexNum num) {
@@ -22,6 +29,9 @@ class ComplexNum {
          * Implementare il metodo in modo che venga aggiunto il numero complesso
          * passato in input - Rif. Appendice A1 (slide)
          */
+
+        this.re += num.re;
+        this.im += num.im;
     }
 
     String toStringRep() {
@@ -29,6 +39,14 @@ class ComplexNum {
          * Implementare il metodo in modo che restituisca una rappresentazione
          * testuale del numero complesso
          */
-        return "";
+
+        String imRep = "";
+        if (this.im > 0) {
+            imRep = String.format("+%.3fi", this.im);
+        } else if (this.im < 0) {
+            imRep = String.format("%.3fi", this.im);
+        }
+
+        return String.format("%.3f%s", this.re, imRep);
     }
 }
