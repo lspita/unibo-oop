42dc751585c8004404883f43852062c7d4082045
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-10-22 15:00:38 +0200

    first exercise

--- oop-lab05/51-set/src/main/java/it/unibo/collections/sets/UseSet.java
+++ oop-lab05/51-set/src/main/java/it/unibo/collections/sets/UseSet.java
@@ -1,6 +1,8 @@
 package it.unibo.collections.sets;
 
 import java.util.Collection;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Example class using {@link java.util.Set}.
@@ -15,7 +17,7 @@ public final class UseSet {
 
     /**
      * @param args
-     *            ignored
+     *             ignored
      */
     public static void main(final String[] args) {
         /*
@@ -24,22 +26,55 @@ public final class UseSet {
          *
          * 1) Builds a TreeSet containing Strings
          */
+
+        final Set<String> set = new TreeSet<>();
+
         /*
-         * 2) Populates such Collection with all the Strings representing numbers ranging from "1" to
+         * 2) Populates such Collection with all the Strings representing numbers
+         * ranging from "1" to
          * "20" (both included)
          */
+
+        for (int i = 0; i < ELEMS; i++) {
+            set.add(Integer.toString(i));
+        }
+
         /*
          * 3) Prints its content
          */
+
+        System.out.println(set.toString());
+
         /*
          * 4) Removes all those strings whose represented number is divisible by three.
          * Note: the method removeIf(Predicate) is not allowed.
          */
+
+        final Set<String> setLoop = new TreeSet<>();
+        setLoop.addAll(set);
+        for (String s : setLoop) {
+            if (Integer.parseInt(s) % 3 == 0) {
+                set.remove(s);
+            }
+        }
+
         /*
          * 5) Prints the content of the Set using a for-each construct
          */
+
+        for (String s : set) {
+            System.out.println(s);
+        }
+
         /*
          * 6) Verifies whether all the numbers left in the set are even
          */
+
+        for (String s : set) {
+            if (Integer.parseInt(s) % 2 == 0) {
+                System.out.println("Found odd number: " + s);
+                return;
+            }
+        }
     }
 }
