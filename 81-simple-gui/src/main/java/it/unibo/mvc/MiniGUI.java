e014cefb17151dbec654f50b1ce881d954aeedba
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-11-12 09:55:26 +0100

    removed unused imports

--- 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 81-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -3,10 +3,8 @@ package it.unibo.mvc;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.JTextPane;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
