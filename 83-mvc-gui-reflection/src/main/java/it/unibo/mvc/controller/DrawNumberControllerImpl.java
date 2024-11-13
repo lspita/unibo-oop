7ab91029bf60e81b4d2524eb31094ffa4ada9996
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-11-13 11:17:25 +0100

    multiple views

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/controller/DrawNumberControllerImpl.java
@@ -4,6 +4,8 @@ import it.unibo.mvc.api.DrawNumber;
 import it.unibo.mvc.api.DrawNumberController;
 import it.unibo.mvc.api.DrawNumberView;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -13,7 +15,7 @@ import java.util.Objects;
 public final class DrawNumberControllerImpl implements DrawNumberController {
 
     private final DrawNumber model;
-    private DrawNumberView view;
+    private List<DrawNumberView> views;
 
     /**
      * Builds a new game controller provided a game model.
@@ -22,22 +24,33 @@ public final class DrawNumberControllerImpl implements DrawNumberController {
      */
     public DrawNumberControllerImpl(final DrawNumber model) {
         this.model = model;
+        this.views = new ArrayList<>();
+    }
+
+    /**
+     * Check that the views collection is in a correct state
+     * @throws IllegalStateException if the are no views attached
+     */
+    public void checkViewsState() throws IllegalStateException {
+        if (this.views.size() == 0) {
+            throw new IllegalStateException("There is no view attached!");
+        }
     }
 
     @Override
     public void addView(final DrawNumberView view) {
         Objects.requireNonNull(view, "Cannot set a null view");
-        if (this.view != null) {
-            throw new IllegalStateException("The view is already set! Multiple views are not supported");
-        }
-        this.view = view;
+        this.views.add(view);
         view.setController(this);
         view.start();
     }
 
     @Override
     public void newAttempt(final int n) {
-        Objects.requireNonNull(view, "There is no view attached!").result(model.attempt(n));
+        this.checkViewsState();
+        for (final DrawNumberView view : this.views) {
+            view.result(model.attempt(n));
+        }
     }
 
     @Override
