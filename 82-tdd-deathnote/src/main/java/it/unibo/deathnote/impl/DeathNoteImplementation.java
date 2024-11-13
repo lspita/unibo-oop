cf9d02bc7a22f5b4d097a66dba49789b372c2561
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-11-13 10:24:08 +0100

    es82 (no final test)

---
+++ 82-tdd-deathnote/src/main/java/it/unibo/deathnote/impl/DeathNoteImplementation.java
@@ -0,0 +1,174 @@
+package it.unibo.deathnote.impl;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import it.unibo.deathnote.api.DeathNote;
+
+/**
+ * Base {@link DeathNote} interface implementation
+ */
+public final class DeathNoteImplementation implements DeathNote {
+    private enum TimedOperation {
+        DEATH_CAUSE(40),
+        DEATH_DETAILS(6040);
+        
+        private long timeout;
+        private long startTime;
+
+        private TimedOperation(final long timeout) {
+            this.timeout = timeout;
+        }
+
+        /**
+         * Check if timed as not timed out
+         * @return `true` if operation has not timed out, `false` otherwise
+         */
+        public boolean isValid() {
+            return System.currentTimeMillis() - this.startTime <= this.timeout;
+        }
+
+        /**
+         * Make opeartion valid for its specific amount of time
+         */
+        public void enable() {
+            this.startTime = System.currentTimeMillis();
+        }
+    }
+
+    private final class DeathInfo {
+        public static String DEFAULT_CAUSE = "heart attack";
+        public static String DEFAULT_DETAILS = "";
+
+        private String cause;
+        private String details;
+        
+        public String getCause() {
+            return cause != null ? cause : DEFAULT_CAUSE;
+        }
+        
+        public String getDetails() {
+            return details != null ? details : DEFAULT_DETAILS;
+        }
+    }
+
+    private Map<String, DeathInfo> deathInfos;
+    private String currentName;
+    
+    /**
+     * Init empty death note
+     */
+    public DeathNoteImplementation() {
+        this.deathInfos = new HashMap<>();
+    }
+
+    /**
+     * Cast rule number to index for the {@link #RULES} list
+     * @param ruleNumber
+     * @return
+     */
+    private static int ruleNumberIndex(final int ruleNumber) {
+        return ruleNumber - 1;
+    }
+
+    /**
+     * Get death infos of current name
+     * @return death infos associated with current name
+     */
+    private DeathInfo getCurrentDeathInfo() {
+        return this.getDeathInfo(this.currentName);
+    }
+
+    /**
+     * Get death infos of given name
+     * @param name name of the dead
+     * @return death infos
+     * @throws IllegalArgumentException if the provider name is not written in this DeathNote.
+     */
+    private DeathInfo getDeathInfo(final String name) throws IllegalArgumentException {
+        if (!this.isNameWritten(name)) {
+            throw new IllegalArgumentException(name + " is not written on the death note");
+        }
+        return this.deathInfos.get(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getRule(int ruleNumber) throws IllegalArgumentException {
+        if (ruleNumber < 1 || ruleNumber > DeathNote.RULES.size()) {
+            throw new IllegalArgumentException("Rule number out of range");
+        }
+        final int index = ruleNumberIndex(ruleNumber);
+        return DeathNote.RULES.get(index);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void writeName(String name) throws NullPointerException {
+        name = Objects.requireNonNull(name);
+        this.currentName = name;
+        this.deathInfos.put(name, new DeathInfo());
+        TimedOperation.DEATH_CAUSE.enable();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean writeDeathCause(String cause) {
+        if (cause == null || this.currentName == null) {
+            throw new IllegalStateException("Invalid state for death cause");
+        }
+        if (!TimedOperation.DEATH_CAUSE.isValid()) {
+            return false;
+        }
+        this.getCurrentDeathInfo().cause = cause;
+        TimedOperation.DEATH_DETAILS.enable();
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean writeDetails(String details) {
+        if (details == null || this.currentName == null) {
+            throw new IllegalStateException("Invalid state for death details");
+        }
+        if (!TimedOperation.DEATH_DETAILS.isValid()) {
+            return false;
+        }
+        this.getCurrentDeathInfo().details = details;
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getDeathCause(String name) {
+        return this.getDeathInfo(name).getCause();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getDeathDetails(String name) {
+        return this.getDeathInfo(name).getDetails();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isNameWritten(String name) {
+        return this.deathInfos.containsKey(name);
+    }
+
+}
--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,109 @@
 package it.unibo.deathnote;
 
-class TestDeathNote {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Set;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.deathnote.api.DeathNote;
+import it.unibo.deathnote.impl.DeathNoteImplementation;
+
+class TestDeathNote {
+    private DeathNote dn;
+    private String mainName = "Nicholas Magi";
+    private String mainDeathCause = "karting accident";
+    private String mainDeathDetails = "Diarrhea";
+    private String otherName = "Matteo Tonelli";
+    private String otherDeathCause = "stomach disease";
+
+    private String DEFAULT_CAUSE = "heart attack";
+    private String DEFAULT_DETAILS = "";
+    private static final long DEATH_CAUSE_TIMEOUT_MS = 1000;
+
+    private Set<Integer> invalidRuleNumbers = Set.of(
+        0, 
+        -1, 
+        DeathNote.RULES.size() + 1
+    );
+
+    @BeforeEach
+    public void init() {
+        this.dn = new DeathNoteImplementation();
+    }
+
+    private void assertNotNullEmptyBlank(final String s) {
+        assertNotNull(s);
+        assertNotEquals(s.trim(), "");
+    }
+
+    /**
+     * Rule number 0 and negative rules do not exist in the DeathNote rules
+     */
+    @Test
+    public void testInvalidRuleNumbers() {
+        for (final int i : invalidRuleNumbers) {
+            assertThrows(
+                IllegalArgumentException.class, 
+                () -> dn.getRule(i)
+            );
+        }
+    }
+
+    /**
+     * No rule is empty or null in the DeathNote rules
+     */
+    @Test
+    public void testRulesNonEmpty() {
+        for (int i = 1; i <= DeathNote.RULES.size(); i++) {
+            assertNotNullEmptyBlank(dn.getRule(i));
+        }
+    }
+
+    /**
+     * The human whose name is written in the DeathNote will eventually die
+     */
+    @Test
+    public void testHumanKill() {
+        assertFalse(dn.isNameWritten(mainName));
+        dn.writeName(mainName);
+        assertTrue(dn.isNameWritten(mainName));
+        assertFalse(dn.isNameWritten(otherName));
+        assertFalse(dn.isNameWritten(""));
+    }
+
+    /**
+     * Only if the cause of death is written within the next 40 milliseconds of 
+     * writing the person's name, it will happen.
+     */
+    @Test
+    public void testCauseOfDeath() throws InterruptedException {
+        assertThrows(
+            IllegalStateException.class, 
+            () -> dn.writeDeathCause(mainDeathCause)
+        );
+        dn.writeName(mainName);
+        assertEquals(dn.getDeathCause(mainName), DEFAULT_CAUSE);
+        dn.writeName(mainName);
+        assertTrue(dn.writeDeathCause(mainDeathCause));
+        assertEquals(dn.getDeathCause(mainName), mainDeathCause);
+        Thread.sleep(DEATH_CAUSE_TIMEOUT_MS);
+        assertFalse(dn.writeDeathCause(otherDeathCause));
+        assertEquals(dn.getDeathCause(mainName), mainDeathCause);
+    }
+
+    /**
+     * Only if the cause of death is written within the next 6 seconds 
+     * and 40 milliseconds of writing the death's details, it will happen
+     */
+    @Test
+    public void testDeathDetails() {
+        // TODO Incoherent
+    }
 }
