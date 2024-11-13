c4fc242b19620ff28268e7f7b709c92cdd2996dd
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-11-13 17:18:14 +0100

    reflection

--- 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,15 +1,25 @@
 package it.unibo.mvc;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.Set;
+
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
-import it.unibo.mvc.view.DrawNumberConsoleView;
-import it.unibo.mvc.view.DrawNumberSwingView;
 
 /**
  * Application entry-point.
  */
 public final class LaunchApp {
+    private static final String VIEWS_PACKAGE_NAME = "it.unibo.mvc.view";
+    private static final Set<String> VIEW_CLASSES = Set.of(
+        "DrawNumberStdoutView",
+        "DrawNumberSwingView"
+    );
+    private static final int VIEW_CLASS_INSTANCES = 3;
 
     private LaunchApp() { }
 
@@ -24,11 +34,29 @@ public final class LaunchApp {
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args) 
+    throws 
+        ClassNotFoundException,
+        NoSuchMethodException,
+        InstantiationException,
+        IllegalAccessException,
+        InvocationTargetException {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberSwingView());
-        app.addView(new DrawNumberConsoleView());
+        for (final String className : VIEW_CLASSES) {
+            final String fullName = VIEWS_PACKAGE_NAME + "." + className;
+            final Class<?> rawClass = Class.forName(fullName);
+            if (!List.of(rawClass.getInterfaces()).contains(DrawNumberView.class)) {
+                throw new IllegalArgumentException(
+                    fullName + "does not implement" + DrawNumberView.class.getName()
+                );
+            }
+            @SuppressWarnings("unchecked") // Implementation of interface is checked before
+            final Class<DrawNumberView> viewClass = (Class<DrawNumberView>)rawClass;
+            Constructor<DrawNumberView> emptyConstructor = viewClass.getConstructor();
+            for (int i = 0; i < VIEW_CLASS_INSTANCES; i++) {
+                app.addView(emptyConstructor.newInstance());
+            }
+        }
     }
 }
