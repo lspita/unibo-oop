7548300151f99e401b7de5469ee2835436a267e1
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-12-03 16:25:53 +0100

    es 11.3

--- 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
+++ 113-reactive-guis/src/main/java/it/unibo/oop/reactivegui03/AnotherConcurrentGUI.java
@@ -18,4 +18,127 @@ import javax.swing.SwingUtilities;
  */
 @SuppressWarnings("PMD.AvoidPrintStackTrace")
 public final class AnotherConcurrentGUI extends JFrame {
+
+    private static final long serialVersionUID = 1L;
+
+    private static final double WIDTH_PERC = 0.2;
+    private static final double HEIGHT_PERC = 0.1;
+    private final JLabel display = new JLabel("0");
+    private final JButton upButton = new JButton("up");
+    private final JButton downButton = new JButton("down");
+    private final JButton stopButton = new JButton("stop");
+
+    private final CounterAgent counterAgent = new CounterAgent();
+    private final TimedStopCounterAgent timedStopCounterAgent = new TimedStopCounterAgent(counterAgent);
+    
+    public AnotherConcurrentGUI() {
+        super();
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setSize((int) (screenSize.getWidth() * WIDTH_PERC), (int) (screenSize.getHeight() * HEIGHT_PERC));
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        final JPanel panel = new JPanel();
+        panel.add(display);
+        panel.add(upButton);
+        panel.add(downButton);
+        panel.add(stopButton);
+        this.getContentPane().add(panel);
+
+        // Handlers
+        upButton.addActionListener(ae -> counterAgent.startIncrementing());
+        downButton.addActionListener(ae -> counterAgent.startDecrementing());
+        stopButton.addActionListener(ae -> counterAgent.stopCounting());
+
+        // Start
+        Thread.ofPlatform().daemon(true).start(counterAgent);
+        Thread.ofPlatform().daemon(true).start(timedStopCounterAgent);
+        this.setVisible(true);
+    }
+
+    /*
+     * The counter agent is implemented as a nested class. This makes it
+     * invisible outside and encapsulated.
+     */
+    private class CounterAgent implements Runnable {
+        
+        private enum Operation {
+            INCREMENT,
+            DECREMENT,
+            STOP
+        }
+
+        private static final long LOOP_TIMEOUT_MS = 100;
+
+        private volatile Operation operation = Operation.STOP;
+        private int counter = 0;
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    this.applyOperation();
+                    final var nextText = Integer.toString(this.counter);
+                    SwingUtilities.invokeAndWait(() -> AnotherConcurrentGUI.this.display.setText(nextText));
+                    Thread.sleep(LOOP_TIMEOUT_MS);
+                } catch (InvocationTargetException | InterruptedException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        
+        private void applyOperation() {
+            switch (this.operation) {
+                case INCREMENT -> this.counter++;
+                case DECREMENT -> this.counter--;
+                default -> {}
+            }
+        }
+
+        /**
+         * External command to start decrementing.
+         */
+        public void startIncrementing() {
+            this.operation = Operation.INCREMENT;
+        }
+
+        /**
+         * External command to start incrementing.
+         */
+        public void startDecrementing() {
+            this.operation = Operation.DECREMENT;
+        }
+
+        /**
+         * External command to stop counting.
+         */
+        public void stopCounting() {
+            this.operation = Operation.STOP;
+        }
+    }
+
+    private class TimedStopCounterAgent implements Runnable {
+
+        private static final long TIMEOUT_MS = 10000;
+
+        private CounterAgent counterAgent;
+
+        public TimedStopCounterAgent(final CounterAgent counterAgent) {
+            this.counterAgent = counterAgent;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(TIMEOUT_MS);
+                this.counterAgent.stopCounting();
+                SwingUtilities.invokeAndWait(() -> {
+                    AnotherConcurrentGUI.this.upButton.setEnabled(false);
+                    AnotherConcurrentGUI.this.downButton.setEnabled(false);
+                    AnotherConcurrentGUI.this.stopButton.setEnabled(false);
+                });
+            } catch (InvocationTargetException | InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
+
