cd45eebcc3e2a03afbb191504b2c834576769ab5
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-12-10 16:30:04 +0100

    es 12.1

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,90 @@
 package it.unibo.es1;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class LogicsImpl implements Logics {
 
+	private class Counter {
+
+		private int value;
+		private final int maxValue;
+
+		public Counter(final int maxValue) {
+			this.maxValue = maxValue;
+		}
+	
+		public int getValue() {
+			return this.value;
+		}
+	
+		public void increment() {
+			if (!this.isEnabled()) {
+				return;
+			}
+
+			this.value++;
+		}
+
+		public boolean isEnabled() {
+			return this.value < this.maxValue; 
+		}
+		
+	}
+
+	private final List<Counter> counters;
+	private final int maxValue;
+
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		this.maxValue = size;
+		this.counters = IntStream.range(0, size)
+							.mapToObj(i -> new Counter(this.maxValue))
+							.collect(Collectors.toList());
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.counters.size();
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return this.counters.stream()
+			.map(Counter::getValue)
+			.collect(Collectors.toList());
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		return this.counters.stream()
+					.map(Counter::isEnabled)
+					.collect(Collectors.toList());
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		final var counter = this.counters.get(elem);
+		counter.increment();
+		return counter.getValue();
 	}
 
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		return this.counters.stream()
+					.mapToInt(Counter::getValue)
+					.mapToObj(Integer::toString)
+					.collect(Collectors.joining("|"));
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		if (this.counters.isEmpty()) {
+			return true;
+		}
+		final var match = Integer.valueOf(this.counters.get(0).getValue());
+		return this.counters.stream()
+				.mapToInt(Counter::getValue)
+				.allMatch(match::equals);
 	}
 }
