94cfa625bd34c40c52a42802f2bd782fff1aa980
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2024-12-11 11:43:38 +0100

    format

--- 122-es2/src/main/java/it/unibo/es2/GUI.java
+++ 122-es2/src/main/java/it/unibo/es2/GUI.java
@@ -2,40 +2,38 @@ package it.unibo.es2;
 
 import javax.swing.*;
 
-import it.unibo.es2.Logics.CellState;
-
 import java.util.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
 public class GUI extends JFrame {
-    
+
     private final Map<JButton, Pair<Integer, Integer>> buttons = new HashMap<>();
     private final Logics logics;
-    
+
     public GUI(int size) {
         this.logics = new LogicsImpl(size);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setSize(100*size, 100*size);
-        
-        JPanel panel = new JPanel(new GridLayout(size,size));
-        this.getContentPane().add(BorderLayout.CENTER,panel);
-        
-        ActionListener al = (e)->{
-            final JButton buttonClicked = (JButton)e.getSource();
-            final Pair<Integer,Integer> buttonPosition = buttons.get(buttonClicked);
+        this.setSize(100 * size, 100 * size);
+
+        JPanel panel = new JPanel(new GridLayout(size, size));
+        this.getContentPane().add(BorderLayout.CENTER, panel);
+
+        ActionListener al = (e) -> {
+            final JButton buttonClicked = (JButton) e.getSource();
+            final Pair<Integer, Integer> buttonPosition = buttons.get(buttonClicked);
             logics.toggle(buttonPosition);
             buttonClicked.setText(getCellString(buttonPosition));
-            if (logics.gameFinished()){
+            if (logics.gameFinished()) {
                 System.exit(1);
-            } 
+            }
         };
-                
-        for (int i=0; i<size; i++){
-            for (int j=0; j<size; j++){
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
                 final JButton jb = new JButton(getCellString(new Pair<>(i, j)));
                 jb.addActionListener(al);
-                this.buttons.put(jb,new Pair<>(i,j));
+                this.buttons.put(jb, new Pair<>(i, j));
                 panel.add(jb);
             }
         }
