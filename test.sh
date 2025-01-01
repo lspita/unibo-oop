f52cef3d3504c0b15ebe74f8e42cdceb163b93a9
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-01-01 23:11:05 +0100

    removed tmp script

--- test.sh
+++
@@ -1,8 +0,0 @@
-BASE_STATS="\n- Time to complete: N/A\n"
-for exam in `find . -mindepth 1 -maxdepth 1 -type d -name "a*"`; do
-    EXAM_RESULTS_FILE=${exam}/README.md
-    echo -e "# Results for `basename ${exam}`\n${BASE_STATS}" > ${EXAM_RESULTS_FILE}
-    for exercise in `find ${exam} -mindepth 1 -maxdepth 1 -type d -name "e*"`; do
-        echo -e "## `basename ${exercise}`\n${BASE_STATS}" >> ${EXAM_RESULTS_FILE}
-    done
-done
