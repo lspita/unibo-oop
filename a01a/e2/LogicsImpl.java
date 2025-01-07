489df01c69c3f68b1688618311bcf022d7476659
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-01-07 16:20:17 +0100

    exam 01-a corrections

--- a01a/e2/LogicsImpl.java
+++ a01a/e2/LogicsImpl.java
@@ -15,11 +15,13 @@ public class LogicsImpl implements Logics {
     private final List<Position> firedCells = new ArrayList<>();
 
     public LogicsImpl(final int size, final int boatSize) {
-        final var boatPivot = new Position(RANDOM.nextInt(size - boatSize), RANDOM.nextInt(size));
+        final var boatPivot = new Position(RANDOM.nextInt(size - boatSize + 1), RANDOM.nextInt(size));
         System.out.println(boatPivot);
-        boat = Stream.iterate(boatPivot.x(), x -> x <= boatSize, x -> x + 1)
+        boat = Stream.iterate(boatPivot.x(), x -> x < boatPivot.x() + boatSize, x -> x + 1)
             .map(x -> new Position(x, boatPivot.y()))
             .toList();
+
+        System.out.println(boat);
     }
 
     @Override
