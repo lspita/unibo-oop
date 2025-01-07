4ddca03d4d5c7e964f7dc766e1b085d16ebf40eb
Ludovico Spitaleri <ludovico.spitaleri@gmail.com>
2025-01-07 11:51:36 +0100

    exam 03-b unused import

--- a03b/e1/LazyTreeFactoryImpl.java
+++ a03b/e1/LazyTreeFactoryImpl.java
@@ -2,7 +2,6 @@ package a03b.e1;
 
 import java.util.Map;
 import java.util.Optional;
-import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
